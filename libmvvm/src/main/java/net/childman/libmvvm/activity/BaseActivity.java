852ff1902fa0e62d9354241849d21a66f291d0e6
czm <czhongm@gmail.com>
2021-01-28 22:35:54 +0800

    fix(lib): 去除onOptionsItemSelected重载

--- libmvvm/src/main/java/net/childman/libmvvm/activity/BaseActivity.java
+++ libmvvm/src/main/java/net/childman/libmvvm/activity/BaseActivity.java
@@ -41,15 +41,15 @@ public abstract class BaseActivity extends RxAppCompatActivity {
         mUiAction = null;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if(id == android.R.id.home){
-            onBackPressed();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//        if(id == android.R.id.home){
+//            onBackPressed();
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 
 
     protected void initUiAction(){
